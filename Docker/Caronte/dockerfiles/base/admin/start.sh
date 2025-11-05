f04a17f8d697ba37b3072794ee11d898511f13a7
morgadodesarrollador <morgadodesarrollador@gmail.com>
2025-11-05 12:20:46 +0100

    proyecto base: imagen base (ubbase)

--- Docker/Caronte/dockerfiles/base/admin/start.sh
+++ Docker/Caronte/dockerfiles/base/admin/start.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-tail -f /dev/null
-## script's que se encargar de configurar el imagen/contenedor
+newUser(){
+    useradd -rm -d /home/morgado -s /bin/bash morgado
+    echo "morgado:1234" | chpasswd
+    echo "Bienvenido Morgado ..." > /home/morgado/bienvenida.txt
+}
+
+main() {
+    newUser
+    # encargada de dejar este contendor vivo en BGround
+    tail -f /dev/null
+    ## script's que se encargar de configurar el imagen/contenedor
+}
+
+main
