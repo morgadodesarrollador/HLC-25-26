43fa19b94d7fe7e611fc992fcdb746759e8f41d2
morgadodesarrollador <morgadodesarrollador@gmail.com>
2025-11-12 12:00:07 +0100

    Creacion de usuario

--- Docker/Caronte/dockerfiles/base/admin/start.sh
+++ Docker/Caronte/dockerfiles/base/admin/start.sh
@@ -2,7 +2,7 @@
 # carga las variables de entono pasadas desde el D.Compose
 set -e
 
-source /root/admin/base/usuarios/mainUsuarios.sh
+#source /root/admin/base/usuarios/mainUsuarios.sh
 
 main() {
     # gestion usuario ---> getsUser.sh
@@ -10,7 +10,7 @@ main() {
     # gestion del ssh ---> gestSsh.sh
     # ...
     touch /root/logs/informe.log
-    newUser
+  #  newUser
     # encargada de dejar este contendor vivo en BGround
     tail -f /dev/null
     ## script's que se encargar de configurar el imagen/contenedor
