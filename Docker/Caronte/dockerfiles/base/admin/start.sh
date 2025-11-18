a727adb128ce35451260c1675db7668201ce6110
morgadodesarrollador <morgadodesarrollador@gmail.com>
2025-11-18 13:19:12 +0100

    Make SSh

--- Docker/Caronte/dockerfiles/base/admin/start.sh
+++ Docker/Caronte/dockerfiles/base/admin/start.sh
@@ -3,7 +3,7 @@
 set -e
 
 source /root/admin/base/usuarios/mainUsuarios.sh
-source /root/admin/base/ssh/mainSSH.sh
+# source /root/admin/base/ssh/mainSSH.sh
 
 # source /root/admin/base/usuarios/makeUsuariosPostgress
 
@@ -18,7 +18,7 @@ main() {
     # then
     #     make_ssh
     # fi
-    
+
     # encargada de dejar este contendor vivo en BGround
     tail -f /dev/null
     ## script's que se encargar de configurar el imagen/contenedor
