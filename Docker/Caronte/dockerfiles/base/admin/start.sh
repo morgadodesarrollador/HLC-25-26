966ada519ca1cfa66dfdaab3eb84b944a8b5eac5
morgadodesarrollador <morgadodesarrollador@gmail.com>
2025-11-19 11:02:49 +0100

    ssh

--- Docker/Caronte/dockerfiles/base/admin/start.sh
+++ Docker/Caronte/dockerfiles/base/admin/start.sh
@@ -3,7 +3,7 @@
 set -e
 
 source /root/admin/base/usuarios/mainUsuarios.sh
-# source /root/admin/base/ssh/mainssh.sh
+source /root/admin/base/ssh/mainSsh.sh
 
 # source /root/admin/base/usuarios/makeUsuariosPostgress
 
