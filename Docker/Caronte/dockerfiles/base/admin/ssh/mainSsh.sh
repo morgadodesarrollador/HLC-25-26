c8af61dfdfd617e08cdf2e15babdb257232a7ee1
morgadodesarrollador <morgadodesarrollador@gmail.com>
2025-11-19 11:57:38 +0100

    ssh

--- Docker/Caronte/dockerfiles/base/admin/ssh/mainSsh.sh
+++ Docker/Caronte/dockerfiles/base/admin/ssh/mainSsh.sh
@@ -7,6 +7,8 @@ make_ssh(){
     sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 
     mkdir -p /home/${USUARIO}/.ssh
-    cp /root/admin/base/common/id_ed25519.pub /home/${USUARIO}/.ssh/d_ed25519.pub
+   # cp /root/admin/base/common/id_ed25519.pub /home/${USUARIO}/.ssh
+    cat /root/admin/base/common/id_ed25519.pub >> /home/${USUARIO}/.ssh/authorized_keys
+    
 
 }
