e762fcab15c47bdbfab87fe9a60df571601dacb4
morgadodesarrollador <morgadodesarrollador@gmail.com>
2025-12-02 10:00:19 +0100

    sweb: nginx

---
+++ Docker/Caronte/dockerfiles/sweb/nginx/admin/start.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+config_nginx(){
+   # Inicia Nginx en segundo plano
+   #Para lanzar Nginx en segundo plano y mantener el contenedor activo, n
+   #ecesitas un proceso en primer plano que evite que Docker finalice el contenedor. 
+   #Nginx, por defecto, se ejecuta como un demonio (en segundo plano), 
+   #pero Docker requiere un proceso principal activo en el contenedor.
+   nginx &
+   # Mantener el contenedor activo ejecutando Nginx en primer plano
+   # exec nginx -g "daemon off;"
+   # Mantén el contenedor vivo
+   #tail -f /dev/null
+}
+
+#....
+
+load_entrypoint_base(){
+   #ejecutar entrypoint ubbase
+   bash /root/admin/base/start.sh
+  
+}
+
+main(){
+   load_entrypoint_base
+   config_nginx
+ 
+   tail -f /dev/null 
+    
+}
+
+main
