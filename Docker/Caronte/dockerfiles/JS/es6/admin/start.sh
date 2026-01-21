4a921739af9703526f3207aa868e33c01a3b7586
morgado <morgadodesarrollador@gmail.com>
2026-01-21 23:58:43 +0100

    deploy mi portfoliio

--- Docker/Caronte/dockerfiles/JS/es6/admin/start.sh
+++ Docker/Caronte/dockerfiles/JS/es6/admin/start.sh
@@ -5,10 +5,12 @@ set -e
 
 main(){
     echo "*** clonando portfolio *******" >> /root/logs/informe.log
-   bash /root/admin/sweb/nginx/admin/start.sh
+   
    cd /var/www/html
    git clone https://github.com/vaibhavtomar04/portfolio.git
+    # bash /root/admin/sweb/nginx/admin/start.sh
 }
+
 
 main
 
