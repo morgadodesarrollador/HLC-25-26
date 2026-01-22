a4401b80ce2aee1961649a8a65b99a72648af7ba
morgado <morgadodesarrollador@gmail.com>
2026-01-22 09:26:25 +0100

    deploy mi portfoliio

--- Docker/Caronte/dockerfiles/JS/es6/admin/start.sh
+++ Docker/Caronte/dockerfiles/JS/es6/admin/start.sh
@@ -8,8 +8,10 @@ main(){
    
    cd /var/www/html
    #git clone https://github.com/vaibhavtomar04/portfolio.git
-   chown -R www-data:www-data /var/www/html/portfolio
-   echo "propiedad del www-data /var/www/html/portfolio..." >> /root/logs/informe.log
+   chown -R www-data:www-data /var/www/html
+#    chown -R www-data:www-data /var/www/html/portfolio
+
+   echo "propiedad del www-data /var/www/html..." >> /root/logs/informe.log
    bash /root/admin/sweb/nginx/admin/start.sh 
 }
 
